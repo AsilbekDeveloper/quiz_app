2b3522301ea30310617b35201f467fd30b326f58
Asilbek Ilhomov <ilhomovasilbek7@gmail.com>
2025-10-09 13:59:04 +0500

    Implemented dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/app.dart';
+import 'package:quiz_app/core/di/service_locator.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setup();
   runApp(const MyApp());
 }
